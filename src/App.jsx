0d7e34e966cdcff6a3fd4a33a3820f87f65103b7
Bruno Sosa <sosabruno3384@gmail.com>
2023-12-25 12:37:52 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import {BrowserRouter, Route, Routes } from 'react-router-dom';
-import About from "./pages/about/About.jsx";
-import Proyects from "./pages/proyects/Proyects.jsx";
 import Header from "./components/header/Header.jsx";
+import About from "./pages/about/About.jsx";
+import Proyects from "./pages/proyects/Proyects";
 import global_en from "./languages/en/translation.json";
 import global_es from "./languages/es/translation.json";
 import i18next from "i18next";
