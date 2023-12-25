f2aa12810c3e017ab433c63ea3ce4c391dc839f5
Bruno Sosa <sosabruno3384@gmail.com>
2023-12-25 13:41:03 -0300

    git ignorecase false

---
+++ src/pages/proyects/Proyects.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./styles.css";
+
+export default function Proyects(){
+
+  return(
+    <section className="proyects--container">
+      <div className="proyect-box">
+        <a href="#">El Buho Web</a>
+      </div>
+      <div className="proyect-box">
+        <a href="#">Platzi Travel</a>
+      </div>
+      <div className="proyect-box">
+        <a href="#">Concesionaria</a>
+      </div>
+      <div className="proyect-box">
+        <a href="#">Porfolio</a>
+      </div>
+
+    </section>
+  );
+}
