26e0cd6faddf842d3fb9fa48ac09359a9e002795
Bruno Sosa <sosabruno3384@gmail.com>
2024-01-08 20:48:46 -0300

    small changes in css

--- src/components/bentos/Introduction.jsx
+++ src/components/bentos/Introduction.jsx
@@ -7,12 +7,18 @@ export default function Introduction() {
 
   return (
     <div className="bento-introduction">
-      <h1>{t('Presentation.Tittle')}</h1>
-      <p>{t('Presentation.Description')}</p>
-      <h3>{t('Education.Tittle')}</h3>
-      {t('Education.Description', { returnObjects: true }).map((item, index) => (
-        <p key={index}>{item}</p>
+      <div>
+        <h1>{t('Presentation.Tittle')}</h1>
+        <p>{t('Presentation.Description')}</p>
+      </div>
+
+      <div>
+        <h3>{t('Education.Tittle')}</h3>
+        {t('Education.Description', { returnObjects: true }).map((item, index) => (
+          <p key={index}>{item}</p>
       ))}
+      </div>
+      
     </div>
   );
 }
